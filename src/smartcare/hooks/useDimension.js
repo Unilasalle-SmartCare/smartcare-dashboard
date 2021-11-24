56f317e455ccc9f3202d2152a3275197e55f2f52
IzauBarbosa <izau.dev@gmail.com>
2021-11-24 19:00:07 -0300

    Fix hook useDimension

--- src/smartcare/hooks/useDimension.js
+++ src/smartcare/hooks/useDimension.js
@@ -1,43 +1,38 @@
-import { useState, useCallback, useEffect } from "react";
+import { useState, useEffect } from "react"
 
 const ORIGIN = Object.freeze({
   width: 0,
   height: 0,
-});
+})
 
 export const useDimension = (ref) => {
 
-  const [dimensionState, setDimensionState] = useState(ORIGIN);
-
-  const setDimension = useCallback(() => {
-    setDimensionState(() => {
-      if (ref && ref.current) {
-        const divRef = ref.current;
-        const dimension = divRef.getBoundingClientRect();
-        
-        return {
-          width: dimension.width,
-          height: dimension.height,
-        }
-      }
-
-      return {
-        width: window.innerWidth,
-        height: window.innerHeight,
-      }
-    });
-  }, [ref]);
+  const [dimensionState, setDimensionState] = useState(ORIGIN)
 
   useEffect(() => {
+    const setDimension = () => {
+    
+      if (ref && ref.current) {
+        const element = ref.current
+        const { width, height, x, y } = element.getBoundingClientRect()
 
-    setDimension();
-    window.addEventListener('resize', setDimension);
+        setDimensionState({ width, height, x, y })
+      } else {
+
+        setDimensionState({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        })
+      }
+    }
+
+    setDimension()
+    window.addEventListener('resize', setDimension)
 
     return (() => {
-      window.removeEventListener('resize', setDimension);
-    });
-
-  }, [setDimension]);
-
-  return [dimensionState];
+      window.removeEventListener('resize', setDimension)
+    })    
+  }, [ref])
+  
+  return dimensionState
 }
